bddc354c7554c679a461c92d61098bfbe869ce75
Lewis McQuillan <lewis.john.mcquillan@gmail.com>
2019-08-12 10:45:14 +0100

    test(querybuilder): fixed QueryBuilder tests to use repository fields parameter

--- test/unit/QueryBuilder.spec.ts
+++ test/unit/QueryBuilder.spec.ts
@@ -1,80 +1,103 @@
 import { Firestore } from '@google-cloud/firestore';
 import { expect } from 'chai';
 import 'mocha';
-import { Collection } from '../../src';
-import { QueryBuilder } from '../../src/utils'; 
+import { Collection, IFieldMeta } from '../../src';
+import { QueryBuilder } from '../../src/utils';
 
 import * as bootstrap from '../../test/bootstrap.spec';
 import Post from '../../test/entities/Post';
+import { getRepository } from '../../src/store';
 
-describe('[unit] QueryBuilder', () => {
-  let firestore : Firestore;
+describe('[unit] QueryBuilder', (): void => {
+  let firestore: Firestore;
+  let fields: Map<string, IFieldMeta>;
 
-  beforeEach(() => {
+  beforeEach((): void => {
     bootstrap.start();
     firestore = bootstrap.getFirestore();
+    fields = getRepository('Post').fields;
   });
 
-  afterEach(() => {
+  afterEach((): void => {
     bootstrap.reset();
   });
 
-  describe('where queries', () => {
-    it('empty where should match firestore base collection query', async () => {
-      const query = QueryBuilder.query(Collection(Post), {
-        where: [],
-      });
+  describe('where queries', (): void => {
+    it('empty where should match firestore base collection query', async (): Promise<void> => {
+      const query = QueryBuilder.query(
+        Collection(Post),
+        fields,
+        {
+          where: [],
+        });
       const firestormResults = await query.get();
-      const firestoreResults = await query.get();
+      const firestoreResults = await firestore.collection('posts').get();
       expect(firestoreResults.docs.length).to.eql(firestormResults.docs.length);
     });
-    it('undefined where should match firestore base collection query', async () => {
-      const query = QueryBuilder.query(Collection(Post), {});
+    it('undefined where should match firestore base collection query', async (): Promise<void> => {
+      const query = QueryBuilder.query(Collection(Post), fields, {});
       const firestormResults = await query.get();
-      const firestoreResults = await query.get();
+      const firestoreResults = await firestore.collection('posts').get();
       expect(firestoreResults.docs.length).to.eql(firestormResults.docs.length);
     });
-    it('single where should match firestore collection query', async () => {
-      const query = QueryBuilder.query(Collection(Post), {
-        where: [
-          ['title', '==', 'test'],
-        ],
-      });
+    it('single where should match firestore collection query', async (): Promise<void> => {
+      const query = QueryBuilder.query(
+        Collection(Post),
+        fields,
+        {
+          where: [
+            ['title', '==', 'test'],
+          ],
+        },
+      );
       const firestormResults = await query.get();
-      const firestoreResults = await query.get();
+      const firestoreResults = await firestore.collection('posts').where('title', '==', 'test').get();
       expect(firestoreResults.docs.length).to.eql(firestormResults.docs.length);
     });
-    it('chain where should match firestore collection query', async () => {
-      const query = QueryBuilder.query(Collection(Post), {
-        where: [
-          ['title', '==', 'test'],
-          ['body', '==', 'test'],
-        ],
-      });
+    it('chain where should match firestore collection query', async (): Promise<void> => {
+      const query = QueryBuilder.query(
+        Collection(Post),
+        fields,
+        {
+          where: [
+            ['title', '==', 'test'],
+            ['body', '==', 'test'],
+          ],
+        },
+      );
       const firestormResults = await query.get();
-      const firestoreResults = await query.get();
+      const firestoreResults = await firestore.collection('posts')
+        .where('title', '==', 'test')
+        .where('body', '==', 'test')
+        .get();
       expect(firestoreResults.docs.length).to.eql(firestormResults.docs.length);
     });
   });
-  
-  describe('order by queries', () => {
-    describe('single order by should match firestore collection query', () => {
-      it('with implicit order direction', async () => {
-        const query = QueryBuilder.query(Collection(Post), {
-          orderBy: [
-            ['title'],
-          ],
-        });
+
+  describe('order by queries', (): void => {
+    describe('single order by should match firestore collection query', (): void => {
+      it('with implicit order direction', async (): Promise<void> => {
+        const query = QueryBuilder.query(
+          Collection(Post),
+          fields,
+          {
+            orderBy: [
+              ['title'],
+            ],
+          });
         const firestormResults = await query.get();
         const firestoreResults = await query.get();
         expect(firestoreResults.docs.length).to.eql(firestormResults.docs.length);
       });
-      it('with explicit order direction', async () => {
-        const query = QueryBuilder.query(Collection(Post), {
-          orderBy: [
-            ['title', 'asc'],
-          ],
-        });
+      it('with explicit order direction', async (): Promise<void> => {
+        const query = QueryBuilder.query(
+          Collection(Post),
+          fields,
+          {
+            orderBy: [
+              ['title', 'asc'],
+            ],
+          });
         const firestormResults = await query.get();
         const firestoreResults = await query.get();
         expect(firestoreResults.docs.length).to.eql(firestormResults.docs.length);
@@ -82,39 +105,51 @@ describe('[unit] QueryBuilder', () => {
     })
   });
 
-  describe('startAt/startAfter queries', () => {
-    it('startAt query should match firestore collection query', async () => {
-      const query = QueryBuilder.query(Collection(Post), {
-        orderBy: [
-          ['title', 'desc']
-        ],
-        startAt: 'Hello World!',
-      });
+  describe('startAt/startAfter queries', (): void => {
+    it('startAt query should match firestore collection query', async (): Promise<void> => {
+      const query = QueryBuilder.query(
+        Collection(Post),
+        fields,
+        {
+          orderBy: [
+            ['title', 'desc']
+          ],
+          startAt: 'Hello World!',
+        },
+      );
       const firestormResults = await query.get();
       const firestoreResults = await query.get();
       expect(firestormResults.docs.length).to.eql(2);
       expect(firestoreResults.docs.length).to.eql(firestormResults.docs.length);
     });
-    it('startAfter query should match firestore collection query', async () => {
-      const query = QueryBuilder.query(Collection(Post), {
-        orderBy: [
-          ['title', 'desc']
-        ],
-        startAfter: 'Hello World!',
-      });
+    it('startAfter query should match firestore collection query', async (): Promise<void> => {
+      const query = QueryBuilder.query(
+        Collection(Post),
+        fields,
+        {
+          orderBy: [
+            ['title', 'desc']
+          ],
+          startAfter: 'Hello World!',
+        },
+      );
       const firestormResults = await query.get();
       const firestoreResults = await query.get();
       expect(firestormResults.docs.length).to.equal(1);
       expect(firestoreResults.docs.length).to.equal(firestormResults.docs.length);
     });
-    it('using startAt and startAfter should use startAt', async () => {
-      const query = QueryBuilder.query(Collection(Post), {
-        orderBy: [
-          ['title', 'desc']
-        ],
-        startAt: 'Hello World!',
-        startAfter: 'Hello World!',
-      });
+    it('using startAt and startAfter should use startAt', async (): Promise<void> => {
+      const query = QueryBuilder.query(
+        Collection(Post),
+        fields,
+        {
+          orderBy: [
+            ['title', 'desc']
+          ],
+          startAt: 'Hello World!',
+          startAfter: 'Hello World!',
+        },
+      );
       const firestormResults = await query.get();
       const firestoreResults = await query.get();
       expect(firestormResults.docs.length).to.eql(2);
@@ -123,39 +158,51 @@ describe('[unit] QueryBuilder', () => {
     });
   });
 
-  describe('endAt/endBefore queries', () => {
-    it('endAt query should match firestore collection query', async () => {
-      const query = QueryBuilder.query(Collection(Post), {
-        orderBy: [
-          ['title', 'desc']
-        ],
-        endAt: 'Hello World!',
-      });
+  describe('endAt/endBefore queries', (): void => {
+    it('endAt query should match firestore collection query', async (): Promise<void> => {
+      const query = QueryBuilder.query(
+        Collection(Post),
+        fields,
+        {
+          orderBy: [
+            ['title', 'desc']
+          ],
+          endAt: 'Hello World!',
+        },
+      );
       const firestormResults = await query.get();
       const firestoreResults = await query.get();
       expect(firestormResults.docs.length).to.eql(1);
       expect(firestoreResults.docs.length).to.eql(firestormResults.docs.length);
     });
-    it('endBefore query should match firestore collection query', async () => {
-      const query = QueryBuilder.query(Collection(Post), {
-        orderBy: [
-          ['title', 'desc']
-        ],
-        endBefore: 'Hello World!',
-      });
+    it('endBefore query should match firestore collection query', async (): Promise<void> => {
+      const query = QueryBuilder.query(
+        Collection(Post),
+        fields,
+        {
+          orderBy: [
+            ['title', 'desc']
+          ],
+          endBefore: 'Hello World!',
+        },
+      );
       const firestormResults = await query.get();
       const firestoreResults = await query.get();
       expect(firestormResults.docs.length).to.equal(0);
       expect(firestoreResults.docs.length).to.equal(firestormResults.docs.length);
     });
-    it('using endAt and endBefore should use endAt', async () => {
-      const query = QueryBuilder.query(Collection(Post), {
-        orderBy: [
-          ['title', 'desc']
-        ],
-        endAt: 'Hello World',
-        endBefore: 'Hello World',
-      });
+    it('using endAt and endBefore should use endAt', async (): Promise<void> => {
+      const query = QueryBuilder.query(
+        Collection(Post),
+        fields,
+        {
+          orderBy: [
+            ['title', 'desc']
+          ],
+          endAt: 'Hello World',
+          endBefore: 'Hello World',
+        },
+      );
       const firestormResults = await query.get();
       const firestoreResults = await query.get();
       expect(firestormResults.docs.length).to.eql(2);
@@ -164,11 +211,15 @@ describe('[unit] QueryBuilder', () => {
     });
   });
 
-  describe('limit query', () => {
-    it('should limit results', async () => {
-      const query = QueryBuilder.query(Collection(Post), {
-        limit: 1,
-      });
+  describe('limit query', (): void => {
+    it('should limit results', async (): Promise<void> => {
+      const query = QueryBuilder.query(
+        Collection(Post),
+        fields,
+        {
+          limit: 1,
+        },
+      );
       const firestormResults = await query.get();
       const firestoreResults = await query.get();
       expect(firestormResults.docs.length).to.eql(1);
